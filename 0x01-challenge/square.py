2d62d881fb6b03e7bf1d3c9cfcd99754c12a9ea3
pretty mohlabe <111237261+9409010617086@users.noreply.github.com>
2023-04-18 09:10:10 +0200

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-class square(): 
+
+class square():
     width = 0
     height = 0
+
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -15,7 +17,8 @@ class square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
-    
+
+
 if __name__ == "__main__":
 
     s = square(width=12, height=9)
